ce0ef27c19657c53dd963a16509d059746f5a437
Guru Ramya <mallelagururamya@gmail.com>
2026-05-24 08:33:38 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,9 +1,5 @@
--- OrgLens DB initialization
--- Tables are created by SQLAlchemy on startup,
--- this file handles extensions and DB-level config.
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pg_trgm";   -- fast LIKE/ILIKE on text columns
+CREATE EXTENSION IF NOT EXISTS "pg_trgm";  
 
--- Useful for full-text search on message content later
 ALTER DATABASE orglens SET default_text_search_config = 'english';
