aa2dd9a1873284e12b11f2b6de0344769da1cabb
Guru Ramya <mallelagururamya@gmail.com>
2026-05-24 08:05:56 +0530

    Update config.py

--- orglens-backend/app/config.py
+++ orglens-backend/app/config.py
@@ -3,36 +3,22 @@ from functools import lru_cache
 from pydantic import Field
 
 class Settings(BaseSettings):
-    # App
     app_name: str = "OrgLens"
     app_version: str = "1.0.0"
     debug: bool = False
     secret_key: str = "change-me-in-production"
-
-    # Database
     database_url: str
-
-    # Redis (general)
     redis_url: str
     GROQ_API_KEY: str = Field(default="", alias="GROQ_API_KEY")
-    # Celery (IMPORTANT ADD THIS)
     celery_broker_url: str
     celery_result_backend: str
-
-    # Slack
     slack_client_id: str = ""
     slack_client_secret : str = ""
     slack_redirect_uri: str = ""
-
-    # Google
     google_client_id: str = ""
     google_client_secret: str = ""
     google_redirect_uri: str = ""
-
-    # CORS
     frontend_url: str = "http://localhost:5173"
-
-    # Uploads
     upload_dir: str = "./uploads"
 
     class Config:
